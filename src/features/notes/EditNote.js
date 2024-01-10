d15216d0f365f6a8a55c85648ce8e519d0eace3c
kwons90 <sanghyukkwon90@gmail.com>
2024-01-10 10:52:40 +0900

    new changes

---
+++ src/features/notes/EditNote.js
@@ -0,0 +1,6 @@
+const EditNote = () => {
+    return (
+        <div>EditUser</div>
+    )
+}
+export default EditNote
---
+++ src/features/users/NewUserForm.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import { useAddNewNoteMutation } from '../notes/notesApiSlice'
+import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave } from '@fortawesome/free-solid-svg-icons'
+import { ROLES } from '../../config/roles'
+
+const USER_REGEX = /^[A-z]{3,20}$/
+const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/
+
+const NewUserForm = () => {
+    return (
+        <div>NewUserForm</div>
+    )
+}
+
+export default NewUserForm
